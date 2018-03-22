5d6950c45cec2cf1591f7a07b91d5a7266847c89
Justin Watts <jcommu@gmail.com>
2018-03-22 10:45:55 -0400

    Add fun new things

--- macstrap.sh
+++ macstrap.sh
@@ -1,38 +1,16 @@
 export ANSIBLE_STDOUT_CALLBACK=debug 
 
-function install_or_update_package {
-    pkg=$1
-    
-    which $pkg >/dev/null
-    if [ $? -ne 0 ] ; then
-        echo "We need to install $pkg!"
-        brew install $pkg
-    else
-        
-        brew ls $pkg >/dev/null
-        if [ $? -ne 0 ] ; then
-            echo "$pkg has to be installed with 'brew'"
-            echo "  a) Remove it and run './cli.sh' again"
-            echo "      or"
-            echo "  b) Manually install it 'brew update && brew install $pkg'"
-            exit 1
-        fi
-        
-        brew outdated $pkg >/dev/null
-        if [ $? -ne 0 ] ; then
-            echo "We need to update $pkg!"
-            brew upgrade $pkg
-        fi
-    fi
-}
-
 which brew > /dev/null 
 if [ $? -ne 0 ] ; then
   echo "We need to install brew!"
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" </dev/null
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 
 fi
 
-install_or_update_package ansible
+which ansible > /dev/null 
+if [ $? -ne 0 ] ; then
+  echo "We need to install ansible!"
+  brew install ansible
+fi
 
 echo 'Rock & Roll, lets do this.'
 ansible-galaxy install --force azohra.macstrap
